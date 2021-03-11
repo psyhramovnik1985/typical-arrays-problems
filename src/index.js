690ffdf95bb2961572575aafd294c45b7892c250
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-03-11 16:44:00 +0300

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    function tt (r,v,i,a) {
+      return r+v;
+    }
+    return array.reduce(tt, 0)/array.length;
+  }
 }
